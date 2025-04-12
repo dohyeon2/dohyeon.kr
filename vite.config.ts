5068eff7496a900204cfab2fd80fba66d0296bd9
Dohyeon Ju <syate@naver.com>
2025-04-13 01:04:19 +0900

    Chore: add tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
-export default {
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
     appType: "spa",
-};
+    plugins: [tailwindcss()],
+});
