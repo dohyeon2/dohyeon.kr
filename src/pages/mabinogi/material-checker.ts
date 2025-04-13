82e65fc934e5ad50ae04df0bdae02f43cf7ca14c
Dohyeon Ju <syate@naver.com>
2025-04-14 04:04:41 +0900

    Feat: material-checker와 material-checklist-section의 완료 상태 관리 기능 추가

--- src/pages/mabinogi/material-checker.ts
+++ src/pages/mabinogi/material-checker.ts
@@ -6,9 +6,13 @@ import { classMap } from "lit/directives/class-map.js";
 import { TailwindElement } from "utilities/TailwindElement";
 import ToastContainer from "../../components/ui/toast-container";
 import "./search-market";
+import { SlChangeEvent } from "@shoelace-style/shoelace";
 
 @customElement("material-checker")
 export class MaterialChecker extends TailwindElement {
+    @property({ type: String })
+    parentPath: string = "";
+
     @property({ type: String })
     material: string = "";
 
@@ -16,21 +20,34 @@ export class MaterialChecker extends TailwindElement {
     amount: number = 0;
 
     @property({ type: Boolean })
-    isCompleted: boolean = false;
-
-    @property({ type: Boolean })
     searchMarket: boolean = false;
 
     @property({ attribute: false })
-    onCompletedChange: ((isCompleted: boolean) => void) | null = null;
+    onChange: ((path: string) => void) | null = null;
+
+    @property({ attribute: false })
+    completedMaterials: Array<string> = [];
+
+    get path() {
+        return [this.parentPath, this.material].filter(Boolean).join(".");
+    }
+
+    private get isAncestorsCompleted() {
+        return this.completedMaterials.some((material) => {
+            return (
+                this.path.startsWith(material) &&
+                material.length < this.path.length
+            );
+        });
+    }
+
+    private get isCompleted() {
+        const isCurrentCompleted = this.completedMaterials.includes(this.path);
+
+        return isCurrentCompleted;
+    }
 
     private searchMarketTimeout: NodeJS.Timeout | null = null;
-
-    updated(changedProperties: Map<string, unknown>) {
-        if (changedProperties.has("isCompleted")) {
-            this.onCompletedChange?.(this.isCompleted);
-        }
-    }
 
     render() {
         const material = materialsData.find((m) => m.name === this.material);
@@ -43,28 +60,28 @@ export class MaterialChecker extends TailwindElement {
 
         const ingredients = material.ingredients ?? [];
 
+        const isCompleted = this.isCompleted || this.isAncestorsCompleted;
+
         return html`<div
             class="flex flex-col gap-2 p-3 border rounded-md border-gray-300 border-solid ${classMap(
                 {
-                    "bg-green-300": this.isCompleted,
-                    "opacity-45": this.isCompleted,
+                    "bg-green-300": isCompleted,
+                    "opacity-45": isCompleted,
                 }
             )}"
         >
             <div class="flex gap-2">
-                <input
-                    type="checkbox"
-                    .checked=${this.isCompleted}
+                <sl-checkbox
                     class="w-6 h-6"
-                    @change=${(e: Event) => {
-                        this.isCompleted = (
-                            e.target as HTMLInputElement
-                        ).checked;
+                    ?checked=${isCompleted}
+                    ?disabled=${this.isAncestorsCompleted}
+                    @sl-change=${(e: SlChangeEvent) => {
+                        this.onChange?.(this.path);
                     }}
-                />
+                > </sl-checkbox>
                 <div
                     class="${classNames({
-                        "line-through opacity-30": this.isCompleted,
+                        "line-through opacity-30": isCompleted,
                     })} text-left flex-1 hover:bg-gray-100 cursor-pointer relative"
                     @click=${() => {
                         ToastContainer.toast(
@@ -115,10 +132,13 @@ export class MaterialChecker extends TailwindElement {
                         ? html`<div class="flex flex-col gap-2">
                               ${ingredients.map((ingredient) => {
                                   return html`<material-checker
-                                      material="${ingredient.name}"
+                                      .material="${ingredient.name}"
                                       .amount="${ingredient.amount *
                                       this.amount}"
-                                      .isCompleted=${this.isCompleted}
+                                      .parentPath="${this.path}"
+                                      .completedMaterials=${this
+                                          .completedMaterials}
+                                      .onChange=${this.onChange}
                                   ></material-checker>`;
                               })}
                           </div>`
