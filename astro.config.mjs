376374bd1d58e165c3e8b49f9be52fd1b4e32633
Dohyeon Ju <syate@naver.com>
2026-01-02 15:30:34 +0900

    Feat: 블로그 시작

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://dohyeon.kr',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		// @ts-ignore
+		plugins: [tailwindcss()],
+	},
 });
